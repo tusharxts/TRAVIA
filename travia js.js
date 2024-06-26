fc53fcecbd1cd517074f7617672472d63f8d1578
Your Name <you@example.com>
2024-06-26 16:01:48 +0530

    TRAVIA

---
+++ travia js.js
@@ -0,0 +1,23 @@
+
+// var ="username";
+// // var password = "password";
+
+
+
+// // function checkPassword() {
+// //     var password = document.getElementById('password').value;
+// //     if (password == '123456') {
+// //         alert('Correct Password');
+// // 		window.location="Untitled-1.html.html"
+// //     } else {
+// //         alert('Wrong Password');
+// //     }
+// // } 
+
+
+
+let ss = document.querySelector("ss");
+ss.onclick = () =>
+{
+    prompt("Please login to continue!! ");
+}
